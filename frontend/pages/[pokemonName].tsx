93c16f2b22c971bbe449feebe099a0c2871a6e13
Aprillion <peter.hozak@gmail.com>
2022-11-27 23:21:50 +0100

    detail WIP

--- frontend/pages/[pokemonName].tsx
+++ frontend/pages/[pokemonName].tsx
@@ -4,15 +4,31 @@ import {useQuery} from '@apollo/client'
 import {DETAIL} from '../apollo'
 import {DetailQuery} from '../__generated__/graphql'
 
+type Props = DetailQuery['pokemonByName']
+
 export default function Detail() {
   const {pokemonName} = useRouter().query
   const {data} = useQuery<DetailQuery>(DETAIL, {variables: {pokemonName}})
-  const pokemon = data?.pokemonByName
+  if (!data) return <main className="detail">Loading...</main>
+
+  const {pokemonByName} = data
+  if (!pokemonByName) return <main className="detail">¯\_(ツ)_/¯</main>
+
+  const {image, name, types, weight, height} = pokemonByName
 
   return (
-    // TODO: finish detail page
-    <div>
-      <pre>{JSON.stringify(pokemon, null, 2)}</pre>
-    </div>
+    // TODO: finish detail page and extract common logic with Card.tsx
+    <main className="detail">
+      {/* eslint-disable-next-line @next/next/no-img-element */}
+      <img src={image.replace(/artwork(.*)\.jpg/, 'artwork/vector$1.png')} alt="" />
+      <h2>{name}</h2>
+      <small>{types.join(', ')}</small>
+      <div>
+        <b>Weight</b>: {weight.minimum} - {weight.maximum}kg
+      </div>
+      <div>
+        <b>Height</b>: {height.minimum} - {height.maximum}kg
+      </div>
+    </main>
   )
 }
